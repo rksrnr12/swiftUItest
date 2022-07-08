a707aecaa320ebe972e768069d832e7cd448ae12
khg <khg@iquest.kr>
2022-07-08 11:29:58 +0900

    Initial Commit

---
+++ test0708/test0708App.swift
@@ -0,0 +1,17 @@
+//
+//  test0708App.swift
+//  test0708
+//
+//  Created by iquest on 2022/07/08.
+//
+
+import SwiftUI
+
+@main
+struct test0708App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
