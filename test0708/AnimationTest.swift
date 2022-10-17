150b0b50559b2876cfa149af833eee70bad83326
khg <khg@iquest.kr>
2022-10-17 15:44:14 +0900

    1017

--- test0708/AnimationTest.swift
+++ test0708/AnimationTest.swift
@@ -14,10 +14,27 @@ struct AnimationTest: View {
     
     var body: some View {
         
-        VStack{
-            if isDetail {
-                detail
-            }else {
+        if isDetail {
+            VStack{
+                Image("dog")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
+                    .matchedGeometryEffect(id: "test", in: test)
+                    .frame(maxWidth: .infinity,maxHeight: 400)
+                    .onTapGesture {
+                        withAnimation {
+                            isDetail.toggle()
+                        }
+                    }.padding()
+            }
+                .frame(maxWidth: .infinity,maxHeight: .infinity)
+                .background(Color("basicColor"))
+                .ignoresSafeArea()
+                //.navigationBarBackButtonHidden(true)
+        }else {
+            VStack{
+                
                 Image("dog")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
@@ -25,24 +42,13 @@ struct AnimationTest: View {
                     .matchedGeometryEffect(id: "test", in: test)
                     .frame(width: 150, height: 150)
                 Spacer()
-            }
-        }.onTapGesture {
-            withAnimation {
-                isDetail.toggle()
-            }
+                
+            }.onTapGesture {
+                withAnimation {
+                    isDetail.toggle()
+                }
+            }.padding()
         }
-        .padding()
-        
-    }
-    
-    var detail: some View {
-        Image("dog")
-            .resizable()
-            .aspectRatio(contentMode: .fill)
-            .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
-            .matchedGeometryEffect(id: "test", in: test)
-            .frame(maxWidth: .infinity,maxHeight: 400)
-        
     }
 }
 
--- test0708/islandTest.swift
+++ test0708/islandTest.swift
@@ -11,54 +11,55 @@ import ActivityKit
 struct islandTest: View {
     var body: some View {
         VStack {
-            Button {
-                test1()
-            } label: {
-                Text("시작")
-            }
-            Button {
-                test2()
-            } label: {
-                Text("업데이트")
-            }
-            Button {
-                test3()
-            } label: {
-                Text("중단")
-            }
+            Text("")
+//            Button {
+//                test1()
+//            } label: {
+//                Text("시작")
+//            }
+//            Button {
+//                test2()
+//            } label: {
+//                Text("업데이트")
+//            }
+//            Button {
+//                test3()
+//            } label: {
+//                Text("중단")
+//            }
 
         }
         .padding()
     }
     
-    func test1() {
-        let testAttributes = Attributes(number: 1, total: "김한국")
-        let testContentState = Attributes.ContentState(testname: "한국", testnum: 10)
-        
-        do {
-            let testActivity = try Activity<Attributes>.request(attributes: testAttributes, contentState: testContentState,pushType: nil)
-            print("다이나믹아일랜드 실행 \(testActivity.id)")
-        }catch(let error){
-            print(error.localizedDescription)
-        }
-    }
-    
-    func test2() {
-        Task{
-            let update = Attributes.ContentState(testname: "업데이트", testnum: 20)
-            for activity in Activity<Attributes>.activities{
-                await activity.update(using: update)
-            }
-        }
-    }
-    
-    func test3() {
-        Task{
-            for activity in Activity<Attributes>.activities{
-                await activity.end(dismissalPolicy: .immediate)
-            }
-        }
-    }
+//    func test1() {
+//        let testAttributes = Attributes(number: 1, total: "김한국")
+//        let testContentState = Attributes.ContentState(testname: "한국", testnum: 10)
+//
+//        do {
+//            let testActivity = try Activity<Attributes>.request(attributes: testAttributes, contentState: testContentState,pushType: nil)
+//            print("다이나믹아일랜드 실행 \(testActivity.id)")
+//        }catch(let error){
+//            print(error.localizedDescription)
+//        }
+//    }
+//
+//    func test2() {
+//        Task{
+//            let update = Attributes.ContentState(testname: "업데이트", testnum: 20)
+//            for activity in Activity<Attributes>.activities{
+//                await activity.update(using: update)
+//            }
+//        }
+//    }
+//
+//    func test3() {
+//        Task{
+//            for activity in Activity<Attributes>.activities{
+//                await activity.end(dismissalPolicy: .immediate)
+//            }
+//        }
+//    }
 }
 
 
